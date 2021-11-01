f9d9e925f019f8d7fe0eb01f66209c5d26a2c844
DoaaDaban <doaadaban993@gmail.com>
2021-11-01 21:03:21 +0200

    solved

--- sort/app/src/test/java/sort/AppTest.java
+++ sort/app/src/test/java/sort/AppTest.java
@@ -6,6 +6,7 @@ package sort;
 
 import org.junit.jupiter.api.Test;
 import sort.insertion.InsertionSort;
+import sort.merge.MergeSort;
 
 import java.util.Arrays;
 
@@ -49,5 +50,52 @@ class AppTest {
         insertionSort.insertionSort(arr4);
         assertEquals("[2, 3, 5, 7, 11, 13]", Arrays.toString(arr4));
 
+    }
+
+    //        <<< COde Challenge 27 >>>
+
+    @Test
+    public void mergeSortTest1(){
+
+        int [] mergeArr1 = {8, 4, 23, 42, 16, 15};
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.mergeSort(mergeArr1);
+        assertEquals("[4, 8, 15, 16, 23, 42]",Arrays.toString(mergeArr1));
+    }
+
+    @Test
+    public void mergeSortTest2(){
+
+        int [] mergeArr2 = {20, 18, 12, 8, 5, -2};
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.mergeSort(mergeArr2);
+        assertEquals("[-2, 5, 8, 12, 18, 20]",Arrays.toString(mergeArr2));
+    }
+
+    @Test
+    public void mergeSortTest3(){
+
+        int [] mergeArr3 = {5, 12, 7, 5, 5, 7};
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.mergeSort(mergeArr3);
+        assertEquals("[5, 5, 5, 7, 7, 12]",Arrays.toString(mergeArr3));
+    }
+
+    @Test
+    public void mergeSortTest4(){
+
+        int [] mergeArr4 = {2, 3, 5, 7, 13, 11,17};
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.mergeSort(mergeArr4);
+        assertEquals("[2, 3, 5, 7, 11, 13, 17]",Arrays.toString(mergeArr4));
+    }
+
+    @Test
+    public void mergeSortTest5(){
+
+        int [] mergeArr5 = {2};
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.mergeSort(mergeArr5);
+        assertEquals("[2]",Arrays.toString(mergeArr5));
     }
 }
